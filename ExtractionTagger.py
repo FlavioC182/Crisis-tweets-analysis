1de8f0b41b5461d7631de450a1452784bef6ae1d
fverg <vergonaf@gmail.com>
2018-05-29 17:08:09 +0200

    Added some comments

--- ExtractionTagger.py
+++ ExtractionTagger.py
@@ -14,6 +14,7 @@ import numpy as np
 from Utilities import selected_Attributes
 from nltk.tag.stanford import CoreNLPNERTagger
 
+
 def extraction_tagger(inputDataSet):
 
     inputDataSet = selected_Attributes(inputDataSet)
@@ -23,7 +24,7 @@ def extraction_tagger(inputDataSet):
     for count in range(0, len(inputDataSet.index)):
         list.append('None')
         booleanList.append(False)
-    #adding 4 columns with default values
+    # adding 4 columns with default values
     inputDataSet["Location"] = pd.Series(list)
     inputDataSet["StateProvince"] = pd.Series(list)
     inputDataSet["Country"] = pd.Series(list)
@@ -35,7 +36,7 @@ def extraction_tagger(inputDataSet):
     inputDataSet["hasCity"] = pd.Series(booleanList)
     inputDataSet["hasCity"] = pd.Series(booleanList)
 
-    #other tags:
+    # other tags:
     inputDataSet["hasTime"] = pd.Series(booleanList)
     inputDataSet["hasCriminal"] = pd.Series(booleanList)
     inputDataSet["hasDuration"] = pd.Series(booleanList)
@@ -55,60 +56,63 @@ def extraction_tagger(inputDataSet):
     inputDataSet = inputDataSet.set_index('TweetID')
     # inputDataSet['tweet_id'] = inputDataSet.index
 
-    #filtering text erasing hashtags:
-    for id,tweet in zip(inputDataSet.index.values,inputDataSet["Text"]):
-        inputDataSet.loc[id,"Text"] = tweet.replace("#","")
+    # filtering text erasing hashtags:
+    for id, tweet in zip(inputDataSet.index.values, inputDataSet["Text"]):
+        inputDataSet.loc[id, "Text"] = tweet.replace("#", "")
 
-    for id, tweet in zip(inputDataSet.index.values,inputDataSet["Text"]):
+    for id, tweet in zip(inputDataSet.index.values, inputDataSet["Text"]):
         Tagger = CoreNLPNERTagger(url='http://localhost:9000/')
         list = Tagger.tag(tweet.split())
 
-        #in order to read the tags from i file
-        with open("Tags.txt",'a') as file:
-            file.write(str(list)+"\n")
+        # in order to read the tags from i file
+        # with open("Tags.txt",'a') as file:
+        # file.write(str(list)+"\n")
 
         for tuple in list:
-            if(tuple[1]=="LOCATION"):
-                inputDataSet.loc[id,"Location"] = tuple[0].lower()
-                inputDataSet.loc[id,"hasLocation"] = True
-            elif(tuple[1]=="STATE_OR_PROVINCE"):
-                inputDataSet.loc[id,"StateProvince"] = tuple[0].lower()
-                inputDataSet.loc[id,"hasStateProvince"] = True
-            elif(tuple[1]=="COUNTRY"):
-                inputDataSet.loc[id,"Country"] = tuple[0].lower()
-                inputDataSet.loc[id,"hasCountry"] = True
-            elif(tuple[1]== "CITY"):
-                inputDataSet.loc[id,"City"] = tuple[0].lower()
-                inputDataSet.loc[id,"hasCity"] = True
-            elif(tuple[1]=="TIME"):
-                inputDataSet.loc[id,"hasTime"] = True
-            elif(tuple[1]=="CRIMINAL_CHARGE"):
-                inputDataSet.loc[id,"hasCriminal"] = True
-            elif(tuple[1]=="DURATION"):
-                inputDataSet.loc[id,"hasDuration"] = True
-            elif(tuple[1]=="DATE"):
-                inputDataSet.loc[id,"hasDate"] = True
-            elif(tuple[1]=="ORGANIZATION"):
-                inputDataSet.loc[id,"hasOrganization"] = True
-            elif(tuple[1]=="CAUSE_OF_DEATH"):
-                inputDataSet.loc[id,"hasDeath"] = True
-            elif(tuple[1]=="MONEY"):
-                inputDataSet.loc[id,"hasMoney"] = True
-            elif(tuple[1]=="IDEOLOGY"):
-                inputDataSet.loc[id,"hasIdeology"] = True
-            elif(tuple[1]=="NUMBER"):
-                inputDataSet.loc[id,"hasNumber"] = True
-            elif(tuple[1]=="RELIGION"):
-                inputDataSet.loc[id,"hasReligion"] = True
-            elif(tuple[1]=="PERSON"):
-                inputDataSet.loc[id,"hasPerson"] = True
-            elif(tuple[1]=="SET"):
-                inputDataSet.loc[id,"hasSet"] = True
+            if(tuple[1] == "LOCATION"):
+                inputDataSet.loc[id, "Location"] = tuple[0].lower()
+                inputDataSet.loc[id, "hasLocation"] = True
+            elif(tuple[1] == "STATE_OR_PROVINCE"):
+                inputDataSet.loc[id, "StateProvince"] = tuple[0].lower()
+                inputDataSet.loc[id, "hasStateProvince"] = True
+            elif(tuple[1] == "COUNTRY"):
+                inputDataSet.loc[id, "Country"] = tuple[0].lower()
+                inputDataSet.loc[id, "hasCountry"] = True
+            elif(tuple[1] == "CITY"):
+                inputDataSet.loc[id, "City"] = tuple[0].lower()
+                inputDataSet.loc[id, "hasCity"] = True
+            elif(tuple[1] == "TIME"):
+                inputDataSet.loc[id, "hasTime"] = True
+            elif(tuple[1] == "CRIMINAL_CHARGE"):
+                inputDataSet.loc[id, "hasCriminal"] = True
+            elif(tuple[1] == "DURATION"):
+                inputDataSet.loc[id, "hasDuration"] = True
+            elif(tuple[1] == "DATE"):
+                inputDataSet.loc[id, "hasDate"] = True
+            elif(tuple[1] == "ORGANIZATION"):
+                inputDataSet.loc[id, "hasOrganization"] = True
+            elif(tuple[1] == "CAUSE_OF_DEATH"):
+                inputDataSet.loc[id, "hasDeath"] = True
+            elif(tuple[1] == "MONEY"):
+                inputDataSet.loc[id, "hasMoney"] = True
+            elif(tuple[1] == "IDEOLOGY"):
+                inputDataSet.loc[id, "hasIdeology"] = True
+            elif(tuple[1] == "NUMBER"):
+                inputDataSet.loc[id, "hasNumber"] = True
+            elif(tuple[1] == "RELIGION"):
+                inputDataSet.loc[id, "hasReligion"] = True
+            elif(tuple[1] == "PERSON"):
+                inputDataSet.loc[id, "hasPerson"] = True
+            elif(tuple[1] == "SET"):
+                inputDataSet.loc[id, "hasSet"] = True
     return inputDataSet
+
 
 # Main method (to use only when this script is launched)
 if __name__ == '__main__':
-    inputDataSet = pd.read_csv('/Users/Flavio/Desktop/Tesi/Progetto/Dataset/2014_California_Earthquake/2014_california_eq.csv',header=0)
-    #erasing useless attributes and changing names
+    inputDataSet = pd.read_csv(
+        '/Users/Flavio/Desktop/Tesi/Progetto/Dataset/2014_California_Earthquake/2014_california_eq.csv', header=0)
+    # erasing useless attributes and changing names
     inputDataSet = extraction_tagger(inputDataSet)
-    inputDataSet.to_csv(r'MetaData/2014_california_Flags_NLP.csv', header=inputDataSet.columns.values, index=True,  sep=',', mode='w')
+    inputDataSet.to_csv(r'MetaData/2014_california_Flags_NLP.csv',
+                        header=inputDataSet.columns.values, index=True,  sep=',', mode='w')
--- MetaDataAddingToTweets_wDF.py
+++ MetaDataAddingToTweets_wDF.py
@@ -12,11 +12,10 @@ from Utilities import str_to_datetime
 from Utilities import deltaSecToInt
 from Utilities import extract_Source
 from Utilities import isRetweet
-pd.options.mode.chained_assignment = None #default='warn'
+pd.options.mode.chained_assignment = None  # default='warn'
 
 
-
-#defined a function in order to exploit it in other scripts if it is useful
+# defined a function in order to exploit it in other scripts if it is useful
 def metaDataExtraction(myDataset):
     keys = json.load(open("keys.json"))
 
@@ -27,36 +26,39 @@ def metaDataExtraction(myDataset):
 
     twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-
-    #ix is used to access to every single value of the column "tweet_id" in order to cancel '
+    # ix is used to access to every single value of the column "tweet_id" in order to cancel '
     #len(myDataset.index) or myDataset["tweet_id"].size
     for count in range(0, len(myDataset.index)):
-        myDataset.loc[count,'tweet_id'] = myDataset.loc[count,'tweet_id'].replace("'","")
+        myDataset.loc[count, 'tweet_id'] = myDataset.loc[count, 'tweet_id'].replace("'", "")
 
     extractedTweets = 0
     not_available = 0
     counter = 0
     firstTweetTime = dt.datetime.max
-    column_names = ['TweetID','CreationTime', 'Followers', 'Followed', 'GeoTagged','TotalTweets', 'TwitterAge',
-                    'nHashTags', 'nMentions','nUrls', 'Verified', 'Label', 'nRetweets', 'nLikes','Source','isRetweet']
+    column_names = ['TweetID', 'CreationTime', 'Followers', 'Followed', 'GeoTagged', 'TotalTweets', 'TwitterAge',
+                    'nHashTags', 'nMentions', 'nUrls', 'Verified', 'Label', 'nRetweets', 'nLikes', 'Source', 'isRetweet']
     endDataset = pd.DataFrame(columns=column_names)
     print("il tipo di endDataset e'")
     print(type(endDataset))
 
-    for id, category in zip(myDataset["tweet_id"],myDataset["choose_one_category"]):
+    for id, category in zip(myDataset["tweet_id"], myDataset["choose_one_category"]):
         while True:
             try:
                 cur_tweet = twitter.show_status(id=id)
-                endDataset.loc[counter]= [id, cur_tweet['created_at'], cur_tweet['user']['followers_count'],
-                cur_tweet['user']['friends_count'], cur_tweet['user']['geo_enabled'],
-                cur_tweet['user']['statuses_count'],usr_age(cur_tweet['user']['created_at']),
-                len(cur_tweet['entities']['hashtags']),len(cur_tweet['entities']['user_mentions']),
-                len(cur_tweet['entities']['urls']),cur_tweet['user']['verified'],category,
-                cur_tweet['retweet_count'],cur_tweet['favorite_count'],extract_Source(str(cur_tweet['source'])),
-                "retweeted_status" in cur_tweet]
+                endDataset.loc[counter] = [id, cur_tweet['created_at'], cur_tweet['user']['followers_count'],
+                                           cur_tweet['user']['friends_count'], cur_tweet['user']['geo_enabled'],
+                                           cur_tweet['user']['statuses_count'], usr_age(
+                                               cur_tweet['user']['created_at']),
+                                           len(cur_tweet['entities']['hashtags']), len(
+                                               cur_tweet['entities']['user_mentions']),
+                                           len(cur_tweet['entities']['urls']
+                                               ), cur_tweet['user']['verified'], category,
+                                           cur_tweet['retweet_count'], cur_tweet['favorite_count'], extract_Source(
+                    str(cur_tweet['source'])),
+                    "retweeted_status" in cur_tweet]
 
-                with open("SourcesPakistan.txt",'a') as file:
-                    file.write(str(cur_tweet['source'])+"\n")
+                # with open("SourcesPakistan.txt",'a') as file:
+                #    file.write(str(cur_tweet['source'])+"\n")
 
                 extractedTweets = extractedTweets + 1
                 print("[DEBUG] Found info for tweet: ", id, ". Added to list.")
@@ -68,7 +70,8 @@ def metaDataExtraction(myDataset):
                 print(e)
                 print("[DEBUG] Maximum number of tweets reached. Trying again in 5 mins...")
                 print("[DEBUG]", extractedTweets, "tweets have been correctly extracted")
-                print("[DEBUG]", not_available, "tweets have encountered problems during download (403, 404, ...)")
+                print("[DEBUG]", not_available,
+                      "tweets have encountered problems during download (403, 404, ...)")
                 for i in range(300):
                     if i == 299:
                         print("[DEBUG]", 300-i, "second left...")
@@ -81,19 +84,22 @@ def metaDataExtraction(myDataset):
                 print("[DEBUG]", e)
                 not_available = not_available + 1
                 break
-        counter = counter +1
+        counter = counter + 1
 
     deltaSec = []
     for date in endDataset["CreationTime"]:
         deltaSec.append(deltaSecToInt(str(str_to_datetime(date)-firstTweetTime)))
     endDataset["DeltaSeconds"] = deltaSec
-    #to avoid the presence of a column of integer indexes in the csv
+    # to avoid the presence of a column of integer indexes in the csv
     endDataset = endDataset.set_index('TweetID')
     return endDataset
 
+
 # Main method (to use only when this script is launched)
 if __name__ == '__main__':
-    myDatasetInput = pd.read_csv('/Users/Flavio/Desktop/Tesi/Progetto/Dataset/2013_Pakistan_eq/2013_pakistan_eq.csv',header=0)
+    myDatasetInput = pd.read_csv(
+        '/Users/Flavio/Desktop/Tesi/Progetto/Dataset/2013_Pakistan_eq/2013_pakistan_eq.csv', header=0)
     endDataset = metaDataExtraction(myDatasetInput)
     col_names = list(endDataset.columns.values)
-    endDataset.to_csv(r'MetaData2/2013_pakistan_metadati_source.csv', header=col_names, index=True, sep=',',mode='w')
+    endDataset.to_csv(r'MetaData2/2013_pakistan_metadati_source.csv',
+                      header=col_names, index=True, sep=',', mode='w')
