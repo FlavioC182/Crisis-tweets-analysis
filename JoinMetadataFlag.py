d4cb6f49879f6670db95f423d203054a038243b7
flavio candela <Flavio@Air-di-flavio.station>
2018-05-30 10:36:34 +0200

    Created irrelevant joins

--- JoinMetadataFlag.py
+++ JoinMetadataFlag.py
@@ -9,7 +9,7 @@ PakistanIrrelevevantMD = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tw
 PakistanFlags = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2013_pakistan_Flags_NLP.csv'
 CaliforniaMetaData = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_california_metadati.csv'
 CaliforniaFlags = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_california_Flags_NLP.csv'
-CaliforniaIrrelevantMD = ''
+CaliforniaIrrelevantMD = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_california_irrelevant_metadati.csv'
 ChileMetaData = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_chile_metadati.csv'
 ChileFlags = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_chile_Flags_NLP.csv'
 ChileIrrelevantMD = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_chile_irrelevant_metadati.csv'
@@ -20,9 +20,9 @@ IndiaFloodsMetaData = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweet
 IndiaFloodsIrrelevantMD = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_india_floods_irrelevant_metadati.csv'
 IndiaFloodsFlags = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_india_Flags_NLP.csv'
 
-MetaDataF = pd.read_csv(MexicoIrrelevantMD, header=0)
-FlagsDataF = pd.read_csv(MexicoFlags, header=0)
+MetaDataF = pd.read_csv(CaliforniaIrrelevantMD, header=0)
+FlagsDataF = pd.read_csv(CaliforniaFlags, header=0)
 
 JoinDataFrame = joinBetweenMetaDataAndCities(MetaDataF, FlagsDataF)
-JoinDataFrame.to_csv(r'Metadata2/2014_odile_hurricane_irrelevant_metadati_Flags.csv',
+JoinDataFrame.to_csv(r'Metadata2/2014_california_irrelevant_metadati_Flags.csv',
                      header=JoinDataFrame.columns.values, index=True, sep=',', mode='w')
