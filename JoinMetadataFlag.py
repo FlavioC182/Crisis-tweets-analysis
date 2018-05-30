a6863d4dda6ea1e65d00b5990785e29932b6bc1e
fverg <vergonaf@gmail.com>
2018-05-30 08:23:13 +0200

    Added links

--- JoinMetadataFlag.py
+++ JoinMetadataFlag.py
@@ -5,16 +5,18 @@ import pandas as pd
 import numpy as np
 from Utilities import joinBetweenMetaDataAndCities
 PakistanMetaData = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2013_pakistan_metadati.csv'
-PakistanIrrelevevantMD = '/Users/Flavio/Desktop/Tesi/Code/MetaDataExtraction/MetaData/2013_pakistan_irrelevant_metadati.csv'
+PakistanIrrelevevantMD = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2013_pakistan_irrelevant_metadati.csv'
 PakistanFlags = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2013_pakistan_Flags_NLP.csv'
 CaliforniaMetaData = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_california_metadati.csv'
 CaliforniaFlags = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_california_Flags_NLP.csv'
-CaliforniaIrrelevantMD = '/Users/Flavio/Desktop/Tesi/Code/MetaDataExtraction/MetaData/2014_california_irrelevant_source_metadati.csv'
-ChileMetaData =
-ChileFlags =
-MetaDataF = pd.read_csv(CaliforniaMetaData, header=0)
-FlagsDataF = pd.read_csv(CaliforniaFlags, header=0)
+CaliforniaIrrelevantMD = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2013_pakistan_irrelevant_metadati.csv'
+ChileMetaData = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_chile_metadati.csv'
+ChileFlags = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_chile_Flags_NLP.csv'
+ChileIrrelevantMD = 'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/MetaData2/2014_chile_irrelevant_metadati.csv'
+
+MetaDataF = pd.read_csv(ChileMetaData, header=0)
+FlagsDataF = pd.read_csv(ChileFlags, header=0)
 
 JoinDataFrame = joinBetweenMetaDataAndCities(MetaDataF, FlagsDataF)
-JoinDataFrame.to_csv(r'Metadata2/2014_california_metadati_Flags.csv',
+JoinDataFrame.to_csv(r'Metadata2/2014_chile_metadati_Flags.csv',
                      header=JoinDataFrame.columns.values, index=True, sep=',', mode='w')
