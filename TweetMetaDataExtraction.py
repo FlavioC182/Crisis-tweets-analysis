818badf39836b459e193de723e995482a4b6eb3a
flavio candela <Flavio@Air-di-flavio.station>
2018-06-03 15:37:51 +0200

    MetaData

--- TweetMetaDataExtraction.py
+++ TweetMetaDataExtraction.py
@@ -97,8 +97,8 @@ def metaDataExtraction(myDataset):
 # Main method (to use only when this script is launched)
 if __name__ == '__main__':
     myDatasetInput = pd.read_csv(
-        'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/CrisisLexT26/2013_LA_airport_shootings/2013_LA_airport_shootings-tweets_labeled.csv', header=0)
+        'https://raw.githubusercontent.com/FlavioC182/Crisis-tweets-analysis/master/CrisisLexT26/2013_Queensland_floods/2013_Queensland_floods-tweets_labeled.csv', header=0)
     endDataset = metaDataExtraction(myDatasetInput)
     col_names = list(endDataset.columns.values)
-    endDataset.to_csv(r'MetaDataFinal/2013_LA_airport_metadati.csv',
+    endDataset.to_csv(r'MetaDataFinal/2013_queensland_floods_metadati.csv',
                       header=col_names, index=True, sep=',', mode='w')
